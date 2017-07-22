56a97090ed3fbfd37efc84facfa228a704ad06c0
Web Freelance 2017-3 <webfreelance2017-3@p2510peolweb711.petrol.local>
2017-07-21 18:45:45 -0700

    styling changes

--- PhotoSite/resources/views/partials/aboutme.blade.php
+++ PhotoSite/resources/views/partials/aboutme.blade.php
@@ -6,8 +6,10 @@
         <h1 class="about-header">About Me</h1>
         <h3 class="about-text">{{ $item->summary }}</h3>
       </div>
-      <div class="col-xs-5 image-wrapper">
-        <img class="gallery-image" src="../assets/about/{{ $item->picture }}">
+      <div class="col-xs-5">
+        <div class="image-wrapper">
+          <img class="gallery-image img-responsive" src="../assets/about/{{ $item->picture }}">
+        </div>
       </div>
     </div>
   @endforeach
